922fdd67fbee98cd36a9449733895fa23a57a122
Rony Alvarez <ronyalvarez@ronys-mbp.home>
2015-09-23 23:30:42 -0400

    Added more cities to the array, still have to add the nickname and complete them.

--- specs.php
+++ specs.php
@@ -11,6 +11,8 @@ Population => 8,491,079
 Area total => 468.9 sq mi
 Rank => 1st, US
 Time zone => Eastern (EST) (UTC-5)
+'Demonym' => 'New Yorker'
+'Nickname' => 'The Big Apple'
 ************************************/
 
 $NewYorkCity = array(
@@ -112,6 +114,416 @@ $SanJose = array(
 	'Time zone' => 'Pacific (UTC-8)',
 	'Demonym' => 'San Josean',
 	'Nickname' => 'The Capital of Sillicon Valley'
+	);
+$Austin = array(
+	'State' => 'Texas',
+	'Name' => 'Austin',
+	'Rank' => '11th, US',
+	'Population' => '842,592',
+	'Area' => '271.8 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Austinite',
+	'Nickname' => ''
+	);
+$Jacksonville = array(
+	'State' => 'Florida',
+	'Name' => 'Jacksonville',
+	'Rank' => '12th, US',
+	'Population' => '836,507',
+	'Area' => '874.6 sq mi',
+	'Time zone' => 'Eastern (EST) (UTC-5)',
+	'Demonym' => 'Jacksonvillian',
+	'Nickname' => ''
+	);
+$Indianapolis = array(
+	'State' => 'Indiana',
+	'Name' => 'Indianapolis',
+	'Rank' => '13th, US',
+	'Population' => '834,852',
+	'Area' => '372 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Indianapolitan',
+	'Nickname' => ''
+	);
+$SanFransisco = array(
+	'State' => 'California',
+	'Name' => 'San Fransisco',
+	'Rank' => '14th, US',
+	'Population' => '825,863',
+	'Area' => '231.89 sq mi ',
+	'Time zone' => 'Pacific Time Zone (UTC−8)',
+	'Demonym' => 'San Franciscan',
+	'Nickname' => ''
+	);
+$Columbus = array(
+	'State' => 'Ohio',
+	'Name' => 'Columbus',
+	'Rank' => '15th, US',
+	'Population' => '809,798',
+	'Area' => '223.11 sq mi ',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Columbusite',
+	'Nickname' => ''
+	);
+$FortWorth = array(
+	'State' => 'Texas',
+	'Name' => 'Fort Worth',
+	'Rank' => '16th, US',
+	'Population' => '777,992',
+	'Area' => '349.2 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Fort Worthian',
+	'Nickname' => ''
+	);
+$Charlotte = array(
+	'State' => 'North Carolina',
+	'Name' => 'Charlotte',
+	'Rank' => '17th, US',
+	'Population' => '775,202',
+	'Area' => '297.7 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Charlottean',
+	'Nickname' => ''
+	);
+$Detroit = array(
+	'State' => 'Michigan',
+	'Name' => 'Detroit',
+	'Rank' => '18th, US',
+	'Population' => '701,475',
+	'Area' => '142.87 sq mi',
+	'Time zone' => 'EST (UTC−5)',
+	'Demonym' => 'Detroiter',
+	'Nickname' => ''
+	);
+$ElPaso = array(
+	'State' => 'Texas',
+	'Name' => 'El Paso',
+	'Rank' => '19th, US',
+	'Population' => '672,538',
+	'Area' => '256.3 sq mi',
+	'Time zone' => 'MST (UTC-7)',
+	'Demonym' => 'El Pasoan',
+	'Nickname' => ''
+	);
+$Memphis = array(
+	'State' => 'Tennessee',
+	'Name' => 'Memphis',
+	'Rank' => '20th, US',
+	'Population' => '655,155',
+	'Area' => '324.0 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Memphian',
+	'Nickname' => ''
+	);
+$Boston = array(
+	'State' => 'Massachusetts',
+	'Name' => 'Boston',
+	'Rank' => '21st, US',
+	'Population' => '636,479',
+	'Area' => '89.63 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Bostonian',
+	'Nickname' => ''
+	);
+$Seattle = array(
+	'State' => 'Washington',
+	'Name' => 'Seattle',
+	'Rank' => '22nd, US',
+	'Population' => '634,535',
+	'Area' => '142.5 sq mi',
+	'Time zone' => 'PST (UTC-8)',
+	'Demonym' => 'Seattleite',
+	'Nickname' => ''
+	);
+$Denver = array(
+	'State' => 'Colorado',
+	'Name' => 'Denver',
+	'Rank' => '23rd, US',
+	'Population' => '634,265',
+	'Area' => '155 sq mi',
+	'Time zone' => 'MST (UTC– 07:00)',
+	'Demonym' => 'Denverite',
+	'Nickname' => ''
+	);
+$Washington = array(
+	'State' => 'DC',
+	'Name' => 'Washington',
+	'Rank' => '24th, US',
+	'Population' => '632,323',
+	'Area' => '68.3 sq mi',
+	'Time zone' => 'EST (UTC−5)',
+	'Demonym' => 'Washingtonian',
+	'Nickname' => ''
+	);
+$Nashville = array(
+	'State' => 'Tennessee',
+	'Name' => 'Nashville-Davidson',
+	'Rank' => '25th, US',
+	'Population' => '624,496',
+	'Area' => '525.94 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Nashvillian',
+	'Nickname' => ''
+	);
+$Baltimore = array(
+	'State' => 'Maryland',
+	'Name' => 'Baltimore',
+	'Rank' => '26th, US',
+	'Population' => '621,342',
+	'Area' => '92.1 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Baltimorean',
+	'Nickname' => ''
+	);
+$LouisvilleJefferson = array(
+	'State' => 'Kentucky',
+	'Name' => 'Louisville, Jefferson',
+	'Rank' => '27th, US',
+	'Population' => '605,110',
+	'Area' => '399 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Louisvillian',
+	'Nickname' => ''
+	);
+$Portland = array(
+	'State' => 'Oregon',
+	'Name' => 'Portland',
+	'Rank' => '28th, US',
+	'Population' => '603,106',
+	'Area' => '145 sq mi',
+	'Time zone' => 'PST (UTC-8)',
+	'Demonym' => 'Portlander',
+	'Nickname' => ''
+	);
+$Oklahoma = array(
+	'State' => 'Oklahoma',
+	'Name' => 'Oklahoma',
+	'Rank' => '29th, US',
+	'Population' => '599,199',
+	'Area' => '620.34 sq mi ',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Oklahoman',
+	'Nickname' => ''
+	);
+$Milwaukee = array(
+	'State' => 'Wisconsin',
+	'Name' => 'Milwaukee',
+	'Rank' => '30th, US',
+	'Population' => '598,916',
+	'Area' => '96.80 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Milwaukeean',
+	'Nickname' => ''
+	);
+$LasVegas = array(
+	'State' => 'Nevada',
+	'Name' => 'Las Vegas',
+	'Rank' => '31st, US',
+	'Population' => '596,424',
+	'Area' => '135.8 sq mi',
+	'Time zone' => 'PST (UTC−8)',
+	'Demonym' => 'Las Vegan',
+	'Nickname' => ''
+	);
+$Albuquerque = array(
+	'State' => 'New Mexico',
+	'Name' => 'Albuquerque',
+	'Rank' => '32nd, US',
+	'Population' => '555,417',
+	'Area' => '189.5 sq mi',
+	'Time zone' => 'MST (UTC-7)',
+	'Demonym' => 'Albuquerquean',
+	'Nickname' => ''
+	);
+$Tucson = array(
+	'State' => 'Arizona',
+	'Name' => 'Tucson',
+	'Rank' => '33rd, US',
+	'Population' => '524,295',
+	'Area' => '236.2 sq mi ',
+	'Time zone' => 'MST (UTC-7)',
+	'Demonym' => 'Tucsonan',
+	'Nickname' => ''
+	);
+$Fresno = array(
+	'State' => 'California',
+	'Name' => 'Fresno',
+	'Rank' => '34th, US',
+	'Population' => '505,882',
+	'Area' => '112.308 sq mi',
+	'Time zone' => 'Pacific (UTC-8)',
+	'Demonym' => 'Fresnan',
+	'Nickname' => ''
+	);
+$Sacramento = array(
+	'State' => 'California',
+	'Name' => 'Sacramento',
+	'Rank' => '35th, US',
+	'Population' => '475,516',
+	'Area' => '100.105 sq mi',
+	'Time zone' => 'Pacific (UTC−8)',
+	'Demonym' => 'Sacramentan',
+	'Nickname' => ''
+	);
+$LongBeach = array(
+	'State' => 'California',
+	'Name' => 'Long Beach',
+	'Rank' => '36th, US',
+	'Population' => '467,892',
+	'Area' => '51.437 sq mi',
+	'Time zone' => 'Pacific (UTC-8)',
+	'Demonym' => 'Long Beacher',
+	'Nickname' => ''
+	);
+$KansasCity = array(
+	'State' => 'Missouri',
+	'Name' => 'Kansas City',
+	'Rank' => '37th, US',
+	'Population' => '464,310',
+	'Area' => '319.03 sq mi',
+	'Time zone' => 'CST (UTC−6)',
+	'Demonym' => 'Kansas Citian',
+	'Nickname' => ''
+	);
+$Mesa = array(
+	'State' => 'Arizona',
+	'Name' => 'Mesa',
+	'Rank' => '38th, US',
+	'Population' => '452,084',
+	'Area' => '133.13 sq mi',
+	'Time zone' => 'MST (UTC-7)',
+	'Demonym' => '',
+	'Nickname' => ''
+	);
+$VirginiaBeach = array(
+	'State' => 'Virginia',
+	'Name' => 'Virginia Beach',
+	'Rank' => '39th, US',
+	'Population' => '447,021',
+	'Area' => '497 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => '',
+	'Nickname' => ''
+	);
+$Atlanta = array(
+	'State' => 'Georgia',
+	'Name' => 'Atlanta',
+	'Rank' => '40th, US',
+	'Population' => '443,775',
+	'Area' => '132.4 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Atlantan',
+	'Nickname' => ''
+	);
+$ColoradoSprings = array(
+	'State' => 'Colorado',
+	'Name' => 'Colorado Springs',
+	'Rank' => '41st, US',
+	'Population' => '431,834',
+	'Area' => '194.7 sq mi',
+	'Time zone' => 'MST (UTC−7)',
+	'Demonym' => '',
+	'Nickname' => ''
+	);
+$Raleigh = array(
+	'State' => 'North Carolina',
+	'Name' => 'Raleigh',
+	'Rank' => '42nd, US',
+	'Population' => '423,179',
+	'Area' => '144.8 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Raleighite',
+	'Nickname' => ''
+	);
+$Omaha = array(
+	'State' => 'Nebraska',
+	'Name' => 'Omaha',
+	'Rank' => '43rd, US',
+	'Population' => '421,570',
+	'Area' => '130.58 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => '',
+	'Nickname' => ''
+	);
+$Miami = array(
+	'State' => 'Florida',
+	'Name' => 'Miami',
+	'Rank' => '44th, US',
+	'Population' => '413,892',
+	'Area' => '55.27 sq mi',
+	'Time zone' => 'EST (UTC-5)',
+	'Demonym' => 'Miamian',
+	'Nickname' => ''
+	);
+$Oakland = array(
+	'State' => 'California',
+	'Name' => 'Oakland',
+	'Rank' => '45th, US',
+	'Population' => '400,740',
+	'Area' => '78.002 sq mi',
+	'Time zone' => 'Pacific (UTC−8)',
+	'Demonym' => 'Oaklander',
+	'Nickname' => ''
+	);
+$Tulsa = array(
+	'State' => 'Oklahoma',
+	'Name' => 'Tulsa',
+	'Rank' => '46th, US',
+	'Population' => '393,987',
+	'Area' => '196.8 sq mi',
+	'Time zone' => 'CST (UTC−6)',
+	'Demonym' => 'Tulsan',
+	'Nickname' => ''
+	);
+$Minneapolis = array(
+	'State' => 'Minnesota',
+	'Name' => 'Minneapolis',
+	'Rank' => '47th, US',
+	'Population' => '392,880',
+	'Area' => '58.4 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Minneapolitan',
+	'Nickname' => ''
+	);
+$Cleveland = array(
+	'State' => 'Ohio',
+	'Name' => 'Cleveland',
+	'Rank' => '48th, US',
+	'Population' => '390,928',
+	'Area' => '82.47 sq mi',
+	'Time zone' => 'EST (UTC−5)',
+	'Demonym' => 'Clevelander',
+	'Nickname' => ''
+	);
+$Wichita = array(
+	'State' => 'Kansas',
+	'Name' => 'Wichita',
+	'Rank' => '49th, US',
+	'Population' => '385,577',
+	'Area' => '163.59 sq mi ',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Wichitan',
+	'Nickname' => ''
+	);
+$Arlington = array(
+	'State' => 'Texas',
+	'Name' => 'Arlington',
+	'Rank' => '50th, US',
+	'Population' => '375,600',
+	'Area' => '99.7 sq mi',
+	'Time zone' => 'CST (UTC-6)',
+	'Demonym' => 'Arlingtonite',
+	'Nickname' => ''
+	);
+$ = array(
+	'State' => '',
+	'Name' => '',
+	'Rank' => '',
+	'Population' => '',
+	'Area' => '',
+	'Time zone' => '',
+	'Demonym' => '',
+	'Nickname' => ''
 	);
 
 
@@ -231,5 +643,15 @@ elseif (strtoupper($city) === 'SAN JOSE') {
 else {
 	echo "Incorrect input, Try again.";
 }
+
+/*
+	for () {
+		
+	}
+*/
+
+
+
+
 
 ?>
